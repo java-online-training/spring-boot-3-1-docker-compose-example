183fa55566ddfc0839f02728deffc462c3cb48c8
Ernie <superernie77@gmail.com>
2023-12-09 20:56:58 +0100

    empty project

---
+++ src/test/java/com/jot/DockerComposeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
